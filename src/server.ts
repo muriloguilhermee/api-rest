84d6ea4e7c83453805fa187fc7e3e4928ad1a2c4
murilloguilherme <murillo.guilherme3101@gmail.com>
2025-11-03 17:29:49 -0300

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,25 @@
-function sum(a: number, b: number): number{
-    return a + b 
-}
+import express from "express"
+import { myMiddleware } from "./middlewares/my-middleware"
 
-const result: number = sum(5,3)
-console.log("Resultado da soma", result)
+const PORT = 3333
+
+const app = express()
+app.use(express.json())
+
+// Middleware global(aplica para todas as rotas abaixo)
+//app.use(myMiddleware)
+
+app.get("/products", (request, response) => {
+    const { page, limit } = request.query
+    response.send(`Pagina ${page} de ${limit}`)
+})
+
+// Middleware local em uma rota especifica.
+app.post("/products", myMiddleware, (request, response) => {
+    const { name, price } = request.body
+
+    // response.send(`Produto ${name} custa ${price}`)
+    response.status(201).json({name, price, user_id: request.user_id})
+})
+
+app.listen(PORT, () => console.log(`Server is running at on port ${PORT}`))
